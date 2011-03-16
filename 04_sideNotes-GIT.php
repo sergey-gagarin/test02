e941445bb198faa91b194dd2e5ad7690be00f3e5
sergey-gagarin <sergboxg@gmail.com>
2011-03-16 16:14:19 +1100

    comm

---
+++ 04_sideNotes-GIT.php
@@ -0,0 +1,42 @@
+<?php
+
+https://github.com/sergey-gagarin/test 
+
+My 
+
+Global setup:
+
+ Download and install Git
+  git config --global user.name "Your Name"
+  git config --global user.email sergboxg@gmail.com
+  Add your public key
+        
+
+Next steps:
+
+  mkdir test
+  cd test
+  git init
+  touch README
+  git add README
+  git commit -m 'first commit'
+  git remote add origin git@github.com:sergey-gagarin/test.git
+  git push -u origin master
+      
+
+Existing Git Repo?
+
+  cd existing_git_repo
+  git remote add origin git@github.com:sergey-gagarin/test.git
+  git push -u origin master
+      
+
+Importing a Subversion Repo?
+
+  Click here
+      
+
+When you're done:
+
+  Continue
+
